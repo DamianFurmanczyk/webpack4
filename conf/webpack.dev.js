7142c6e1630558889168103a27a541b7a4e15d6a
damianfurmanczyk <DamianFurmanczykGM@gmail.com>
2018-05-24 20:52:02 +0200

    stage 2

--- conf/webpack.dev.js
+++ conf/webpack.dev.js
@@ -1,9 +1,29 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 const css = {
   test: /\.css$/,
-  use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+  use: [
+    {
+      loader: "style-loader"
+    }, {
+      loader: "css-loader"
+    }
+  ]
 };
+
+const js = {
+  test: /\.js$/,
+  use: [
+    {
+      loader: "babel-loader",
+      exclude: /node_modules/,
+      options: {
+        presets: ["es2015"]
+      }
+    }
+  ]
+}
 
 const htmlRules = {
   test: /\.html$/,
@@ -13,15 +33,65 @@ const htmlRules = {
       options: {
         name: "[name].html"
       }
-    },
-    { loader: "extract-loader" },
-    { loader: "html-loader" }
+    }, {
+      loader: "extract-loader"
+    }, {
+      loader: "html-loader",
+      options: {
+        attrs: ["img:src"]
+      }
+    }
   ]
+};
+
+const styl = {
+  test: /\.styl$/,
+  use: [
+    {
+      loader: "style-loader"
+    }, {
+      loader: "css-loader"
+    }, {
+      loader: "stylus-loader"
+    }
+  ]
+};
+
+const sass = {
+  test: /.\sass$/,
+  use: [
+    {
+      loader: 'style-loader'
+    }, {
+      loader: 'css-loader'
+    }, {
+      loader: 'sass-loader'
+    }
+  ]
+};
+
+const pug = {
+  test: /\.pug$/,
+  use: [
+    {
+      loader: "pug-loader"
+    }
+  ]
+}
+
+const images = {
+  test: /\.(jpg|gif|png)$/,
+  use: {
+    loader: "file-loader",
+    options: {
+      name: "imgs/[name].[ext]"
+    }
+  }
 };
 
 module.exports = {
   entry: {
-    main: "./src/main.js"
+    main: ["core-js/fn/promise", "./src/main.js"]
   },
   mode: "development",
   output: {
@@ -31,9 +101,19 @@ module.exports = {
   },
   devServer: {
     contentBase: "dist",
-    overlay: true
+    overlay: true,
+    port: 3000
   },
+  devtool: "source-map",
   module: {
-    rules: [css, htmlRules]
-  }
+    rules: [
+      css,
+      styl,
+      sass,
+      htmlRules,
+      pug,
+      images
+    ]
+  },
+  plugins: [new htmlWebpackPlugin({template: './src/index.pug', title: "Boilerplatee"})]
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-require("./main.css");
-require("./index.html");
-
+require("babel-register");
+// import "./main.css" import "./style.sass"
+import "./style.styl"
+import haha from "./index.pug"
+// require("./index.html");
+console.log(haha);
 console.log("Aaa");
+
+let a = 1,
+  b = 3;
+
+[a, b] = [b, a]
+
+let af = async() => {
+
+  await console.log(a, b);
+}
+
+// debugger;
+
+af();
